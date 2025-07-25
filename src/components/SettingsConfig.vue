1a143ca879cf1e129ffd03754317e148391b93ac
Merk42 <mark_c@markecurtis.com>
2025-07-25 18:40:03 -0400

    settings checkboxes

--- src/components/SettingsConfig.vue
+++ src/components/SettingsConfig.vue
@@ -43,16 +43,29 @@ function cancelChanges() {
     <CommonButton @click="openSettings()">settings</CommonButton>
     <DialogModal ref="configDialog" :confirm-text="'save'" :show-cancel="true" @confirm="saveChanges()"
         @cancel="cancelChanges()" title="settings">
-        <label><input type="checkbox" v-model="settings.shake" /> shake screen on hit</label>
-        <label><input type="checkbox" v-model="settings.vibrate" /> vibrate device on hit</label>
-        <label><input type="checkbox" v-model="settings.animate" /> animated water</label>
-        <label><input type="checkbox" v-model="settings.sound" /> sound</label>
+        <label><input type="checkbox" class="sr-only" v-model="settings.shake" /> shake screen on hit</label>
+        <label><input type="checkbox" class="sr-only" v-model="settings.vibrate" /> vibrate device on hit</label>
+        <label><input type="checkbox" class="sr-only" v-model="settings.animate" /> animated water</label>
+        <label><input type="checkbox" class="sr-only" v-model="settings.sound" /> sound</label>
     </DialogModal>
 </template>
 <style scoped>
 label {
     display: block;
     padding: .25rem;
+    border: 2px solid transparent;
     font-size: 1rem;
+    margin: .25rem 0;
+    padding: .25rem .5rem;
+}
+
+label:hover {
+    cursor: pointer;
+}
+
+label:has(:checked) {
+    background-color: orange;
+    border: 2px solid yellow;
+    color: #000;
 }
 </style>
