baab078eff757fc5eff9e9e41e3386e2db8e4be2
Merk42 <mark_c@markecurtis.com>
2025-07-27 10:58:16 -0400

    bombs?

--- src/components/AmmoDisplay.vue
+++ src/components/AmmoDisplay.vue
@@ -2,45 +2,43 @@
 import { computed } from 'vue';
 
 const props = defineProps<{
-    total: number;
-    remaining: number;
+  total: number;
+  remaining: number;
 }>();
 
 const used = computed(() => {
-    return props.total - props.remaining;
+  return props.total - props.remaining;
 })
 </script>
 <template>
-    <div>
-        <template v-for="x in total">
-            <span :class="{ 'index': (x - 1) === used }"></span>
-        </template>
-    </div>
+  <div>
+    <template v-for="x in total" :key="x">
+      <span :class="{ 'index': (x - 1) === used }"></span>
+    </template>
+  </div>
 </template>
 <style scoped>
 div {
-    display: grid;
-    grid-template-columns: repeat(3, minmax(0, 1fr));
-    width: 150px;
+  display: grid;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
 }
 
 @media (orientation: portrait) {
-    div {
-        grid-template-columns: repeat(8, minmax(0, 1fr));
-        width: 400px;
-    }
+  div {
+    grid-template-columns: repeat(8, minmax(0, 1fr));
+  }
 }
 
 span {
-    display: inline-block;
-    width: 50px;
-    height: 50px;
-    background-image: url('/bomb.svg');
-    filter: grayscale(1) contrast(0.65);
+  display: inline-block;
+  aspect-ratio: 1;
+  background-image: url('/bomb.svg');
+  filter: grayscale(1) contrast(0.65);
+  background-size: contain;
 }
 
 .index,
 .index~span {
-    filter: grayscale(0) contrast(1);
+  filter: grayscale(0) contrast(1);
 }
 </style>
--- src/components/TargetDisplay.vue
+++ src/components/TargetDisplay.vue
@@ -1,61 +1,61 @@
 <script setup lang="ts">
-const props = defineProps<{
-    targets: Array<{ name: string, length: number }>
+defineProps<{
+  targets: Array<{ name: string, length: number }>
 }>();
 </script>
 <template>
-    <div id="targetcontainer">
-        <template v-for="target in targets">
-            <div class="target" :class="{ 'dead': !target.length }">
-                <div class="squid"></div>
+  <div id="targetcontainer">
+    <template v-for="target in targets" :key="target.name">
+      <div class="target" :class="{ 'dead': !target.length }">
+        <div class="squid"></div>
 
-                <div class="explosion"></div>
-            </div>
-        </template>
-    </div>
+        <div class="explosion"></div>
+      </div>
+    </template>
+  </div>
 </template>
 <style lang="css" scoped>
 .target,
 .explosion,
 .squid {
-    width: 74px;
-    aspect-ratio: 1;
-    background-size: cover;
+  width: 74px;
+  aspect-ratio: 1;
+  background-size: cover;
 }
 
 .target {
-    display: grid;
+  display: grid;
 }
 
 .explosion {
-    background-image: url('/kaboom.svg');
-    transform: scale(0);
-    transition: transform .2s;
+  background-image: url('/kaboom.svg');
+  transform: scale(0);
+  transition: transform .2s;
 }
 
 .squid {
-    background-image: url('/squid.svg');
-    transition: filter .2s;
+  background-image: url('/squid.svg');
+  transition: filter .2s;
 }
 
 .explosion,
 .squid {
-    grid-row: 1;
-    grid-column: 1;
+  grid-row: 1;
+  grid-column: 1;
 }
 
 .dead .explosion {
-    transform: scale(1);
+  transform: scale(1);
 }
 
 .dead .squid {
-    filter: brightness(0.45) saturate(10) hue-rotate(130deg);
+  filter: brightness(0.45) saturate(10) hue-rotate(130deg);
 }
 
 
 @media (orientation: portrait) {
-    #targetcontainer {
-        display: flex;
-    }
+  #targetcontainer {
+    display: flex;
+  }
 }
 </style>
