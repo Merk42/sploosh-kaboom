7e25f40890d9fa81c65a7993b17e020983592d21
Merk42 <mark_c@markecurtis.com>
2025-08-07 10:21:01 -0400

    fix icons

--- vite.config.ts
+++ vite.config.ts
@@ -3,57 +3,58 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-      VitePWA({
-        registerType:'prompt',
-        includeAssets:['favicon.ico', "apple-touch-icon.png"],
-        manifest:{
-          name:"Sploosh Kaboom",
-          short_name:"Sploosh",
-          description:"Squid Hunt game from The Legend of Zelda: The Wind Waker",
-          icons:[{
-            src: '/icons/android-chrome-192x192.png',
-            sizes:'192x192',
-            type:'image/png',
-            purpose:'favicon'
+    VitePWA({
+      registerType: 'prompt',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Sploosh Kaboom',
+        short_name: 'Sploosh',
+        description: 'Squid Hunt game from The Legend of Zelda: The Wind Waker',
+        icons: [
+          {
+            src: '/etc/sploosh-kaboom/icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
           },
           {
-            src:'/icons/android-chrome-512x512.png',
-            sizes:'512x512',
-            type:'image/png',
-            purpose:'favicon'
+            src: '/etc/sploosh-kaboom/icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
           },
           {
-            src: '/icons/apple-touch-icon.png',
-            sizes:'180x180',
-            type:'image/png',
-            purpose:'apple touch icon',
+            src: '/etc/sploosh-kaboom/icons/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'any',
           },
           {
-            src: '/icons/maskable_icon.png',
-            sizes:'512x512',
-            type:'image/png',
-            purpose:'any maskable',
-          }
+            src: '/etc/sploosh-kaboom/icons/monochrome.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'monochrome maskable',
+          },
         ],
-        theme_color:'#001e34',
-        background_color:'#001e34',
-        display:"standalone",
-        scope:'/etc/sploosh-kaboom',
-        start_url:"/etc/sploosh-kaboom/index.html"
-      }
-    }) 
+        theme_color: '#001e34',
+        background_color: '#001e34',
+        display: 'standalone',
+        scope: '/etc/sploosh-kaboom',
+        start_url: '/etc/sploosh-kaboom/index.html',
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/etc/sploosh-kaboom/'
+  base: '/etc/sploosh-kaboom/',
 })
